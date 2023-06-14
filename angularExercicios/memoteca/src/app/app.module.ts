a70384558e3ef2f63fadf4aa581db2b6ff16c084
Roninho514 <roni.silvasantos514@gmail.com>
2023-06-13 23:01:19 -0300

    inicio curso angular CRUD

---
+++ angularExercicios/memoteca/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CabecalhoComponent,
+    RodapeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
